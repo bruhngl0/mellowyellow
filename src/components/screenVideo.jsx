fd60a6c123139ee0acff6cded27e5d5c36a35e7c
bruhngl0 <adityashrm500@gmail.com>
2025-01-24 23:33:23 +0530

    prop

--- src/components/screenVideo.jsx
+++ src/components/screenVideo.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../styles/screenvideo.scss';
+import '../styles/screenVideo.scss';
 import Header from './Header';
 
 const ScreenVideo = () => {
