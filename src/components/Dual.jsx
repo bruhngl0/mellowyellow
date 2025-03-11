1ea86053650c873f725e3afc824ea59a2c33e678
bruhngl0 <adityashrm500@gmail.com>
2025-03-11 20:54:32 +0530

    duo

--- src/components/Dual.jsx
+++ src/components/Dual.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import Fancy from './Fancy'
 import '../styles/dual.scss'
 
 const Dual = () => {
+  // Define animation variants
+  const variants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.6,
+        ease: [0.22, 1, 0.36, 1],
+      },
+    },
+  }
+
   return (
     <div className='dual-main'>
-        <div className='dual-left'>
-           
-            <img src  = "candle.jpg" />
-        </div>
-        <div className='dual-right'>
+        <motion.div className='dual-left' variants={variants} initial="hidden" whileInView="visible">
+            <img src="candle.jpg" alt="Candle" />
+        </motion.div>
+        <motion.div className='dual-right' variants={variants} initial="hidden" whileInView="visible">
             <Fancy />
-           
-        </div>
-
+        </motion.div>
     </div>
   )
 }
